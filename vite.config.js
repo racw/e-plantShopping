760f66f2a47bdefb2cac2fef75f2dce16afad41b
Ricky <ricky@rschooltoday.com>
2025-04-01 19:54:31 +0700

    update package and vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/shoppingreact",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust chunk size warnings
+    minify: 'esbuild', // Faster minification
+    sourcemap: true, // Enable source maps for debugging
+  },
 })
