874c66b9fe488ed4e740dca766d2c6640b209237
Ricky <ricky@rschooltoday.com>
2025-04-01 21:03:01 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping",
+  base: "/e-plantShopping/",  // Added trailing slash
   plugins: [react()],
 })
